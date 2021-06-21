89e8e15455cc03730893ebe97e89891bfaaa7abc
Yuri <Yuri@resico.cn>
2021-06-21 14:24:17 +0800

    Id1190 Id461 Id852 Id374 Id877 Id483

--- src/main/java/main/LeetCode.java
+++ src/main/java/main/LeetCode.java
@@ -13,10 +13,202 @@ import java.util.stream.Collectors;
 
 public class LeetCode {
     public static void main(String[] args) {
+//        int[] candiesCount = new int[]{7, 4, 5, 3, 8};
+//        int[][] queries = new int[][]{{0, 2, 2}, {4, 2, 4}, {2, 13, 1000000000}};
+//        Id1744 id1744 = new Id1744();
+//        id1744.canEat(candiesCount, queries);
+
+
 
     }
 }
 
+class Id483 {
+
+
+
+    //二分法
+    public long pow(long mid, int j) {
+        long res = mid;
+        for (int i = 1; i < j; i++) {
+            if (Long.MAX_VALUE / mid < res) { //防止超出long上限
+                return -1;
+            }
+            res *= mid;
+        }
+        return res;
+    }
+
+    public long calculate(long mid, int i) {
+        long sum = 1;
+        for (int j = i - 1; j > 0; j--) {
+            long p ;
+            if ((p = pow(mid, j)) == -1) {
+                return -1;
+            }
+            sum += p;
+        }
+        return sum;
+    }
+
+    public String smallestGoodBase(String n) {
+        long num = Long.parseLong(n), mid;
+        for (int i = 63; i >= 2; i--) {
+            long l = num, r = 2;
+            while (r < l) {
+                mid = l + r >>> 1;
+                long sum = calculate(mid, i);
+                if (sum == num) {
+                    return String.valueOf(mid);
+                } else if (sum > num || sum == -1) {
+                    l = mid;
+                } else {
+                    r = mid + 1;
+                }
+            }
+        }
+        return String.valueOf(0);
+    }
+}
+
+class Id877 {
+    //博弈论 先手必然可以选择奇偶序列
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+
+}
+
+class Id374 {
+    //guess为系统内置函数
+    int guess(int num) {
+        int pick = 1702766719;
+        return Integer.compare(pick, num);
+    }
+
+    //二分法 log(n)
+    public int guessNumber(int n) {
+        int mid = 0, ans = 1, a = 0, b = n;
+        while (ans != 0) {
+            mid = a + b >>> 1;
+            ans = guess(mid);
+            if (ans == 1) {
+                a = mid + 1;
+            } else if (ans == -1) {
+                b = mid - 1;
+            }
+        }
+        return mid;
+    }
+}
+
+class Id852 {
+    //枚举 n
+    public int peakIndexInMountainArray(int[] arr) {
+        List<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toList());
+        return list.indexOf(Collections.max(list));
+    }
+    //二分法 log(n)
+}
+
+class Id1744 {
+    public boolean[] canEat(int[] candiesCount, int[][] queries) {
+        int n = candiesCount.length;
+        boolean[] res = new boolean[queries.length];
+        long[] sum = new long[n + 1];
+        sum[0] = 0; //在吃第几类前，需要吃的糖数（前面几类总和）
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + candiesCount[i - 1];
+        }
+        for (int i = 0; i < queries.length; i++) {
+            //ft最爱的糖 fd最爱的天数 dc一天最多吃几个
+            int ft = queries[i][0], fd = queries[i][1], dc = queries[i][2];
+            long x1 = fd + 1;
+            long y1 = (long) dc * fd + dc;
+            long x2 = sum[ft] + 1;
+            long y2 = sum[ft + 1];
+            res[i] = y2 > x1 && y1 > x2;
+        }
+        return res;
+    }
+}
+
+class Id461 {
+    /**
+     * 异或运算
+     */
+    public int hammingDistance(int x, int y) {
+        int n = x ^ y, res = 0;
+        String s = Integer.toBinaryString(n);
+        int k;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    //官方 内置位计数功能
+    public int hammingDistanceOfficially(int x, int y) {
+        return Integer.bitCount(x ^ y);
+    }
+
+    //官方2 开始想出来没实现的思路 移位实现位计数
+    public int hammingDistanceOfficially2(int x, int y) {
+        int z = x ^ y;
+        int sum = 0;
+        while (z != 0) {
+            sum += z & 1;
+            z = z >> 1;
+        }
+        return sum;
+    }
+
+    //官方3 Brian Kernighan算法 每次s &= s - 1 就删除了s二进制最右边的1
+    public int hammingDistanceOfficially3(int x, int y) {
+        int s = x ^ y, ret = 0;
+        while (s != 0) {
+            s &= s - 1;
+            ret++;
+        }
+        return ret;
+    }
+}
+
+class Id1190 {
+    /**
+     * 字符串处理
+     * 模拟
+     */
+    //官方题解 预处理括号
+    public String reverseParentheses(String s) {
+        int n = s.length();
+        int[] num = new int[n];
+        Deque<Integer> stack = new LinkedList<>();
+        for (int i = 0, j; i < n; i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else if (s.charAt(i) == ')') {
+                j = stack.pop();
+                num[i] = j;
+                num[j] = i;
+            }
+        }
+        int index = 0, k = 1;
+        StringBuilder sb = new StringBuilder();
+        while (index < n) {
+            if (s.charAt(index) == '(' || s.charAt(index) == ')') {
+                index = num[index];
+                k = -k;
+            } else {
+                sb.append(s.charAt(index));
+            }
+            index += k;
+        }
+        return sb.toString();
+    }
+}
 
 class Id664 {
     /**
@@ -32,7 +224,7 @@ class Id664 {
                 if (s.charAt(i) == s.charAt(j)) {
                     ary[j][i] = ary[j + 1][i];
                 } else {
-                    for (int k = i; k > j ; k--) {
+                    for (int k = i; k > j; k--) {
                         minn = Math.min(minn, ary[j][k - 1] + ary[k][i]);
                     }
                     ary[j][i] = minn;
