d9f287bf8b70b687a5ba70f437ef1658a15f4a77
Yuri <AkimotoAkiraXs@gmail.com>
2022-07-28 10:36:15 +0800

    1331 数组序号转换_数字简单模拟

---
+++ src/main/java/leetcode/editor/cn/FractionAdditionAndSubtraction.java
@@ -0,0 +1,94 @@
+//给定一个表示分数加减运算的字符串 expression ，你需要返回一个字符串形式的计算结果。 
+//
+// 这个结果应该是不可约分的分数，即最简分数。 如果最终结果是一个整数，例如 2，你需要将它转换成分数形式，其分母为 1。所以在上述例子中, 2 应该被转换为
+// 2/1。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: expression = "-1/2+1/2"
+//输出: "0/1"
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: expression = "-1/2+1/2+1/3"
+//输出: "1/3"
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: expression = "1/3-1/2"
+//输出: "-1/6"
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 输入和输出字符串只包含 '0' 到 '9' 的数字，以及 '/', '+' 和 '-'。 
+// 输入和输出分数格式均为 ±分子/分母。如果输入的第一个分数或者输出的分数是正数，则 '+' 会被省略掉。 
+// 输入只包含合法的最简分数，每个分数的分子与分母的范围是 [1,10]。 如果分母是1，意味着这个分数实际上是一个整数。 
+// 输入的分数个数范围是 [1,10]。 
+// 最终结果的分子与分母保证是 32 位整数范围内的有效整数。 
+// 
+//
+// Related Topics 数学 字符串 模拟 👍 105 👎 0
+
+
+package leetcode.editor.cn;
+
+/**
+ * Id：&emsp;&emsp;592
+ * <p>
+ * Name：分数加减运算
+ *
+ * @author Yuri
+ * @since 2022-07-27 17:50:31
+ */
+public class FractionAdditionAndSubtraction {
+    public static void main(String[] args) {
+        Solution solution = new FractionAdditionAndSubtraction().new Solution();
+        System.out.println();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String fractionAddition(String expression) {
+            StringBuilder sb = new StringBuilder(expression);
+            int firstFlag = 1;
+            if (sb.charAt(0) == '-') {
+                firstFlag = -1;
+                sb.deleteCharAt(0);
+                
+            }
+            int lcm = 1;
+            for (int i = 2; i < sb.length(); i += 3) {
+                lcm = lcm(lcm, sb.charAt(i) - '0');
+            }
+            for (int i = 0; i < sb.length(); i += 3) {
+
+            }
+
+            return null;
+        }
+
+
+        private int lcm(int a, int b) {
+            if (a * b == 0) return 0;
+            return (a * b) / gcd(a, b);
+        }
+
+        private int gcd(int a, int b) {
+            if (b == 0) return a;
+            return gcd(b, a % b);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
