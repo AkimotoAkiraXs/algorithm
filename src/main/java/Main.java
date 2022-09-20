a02957cd574eb7ee85466696f7e76955839dd741
Yuri <akimotoakiraxs@gmail.com>
2022-09-20 11:08:56 +0800

    151 反转字符串中的单词_Api调用处理字符串

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,20 @@
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 /**
  * @author Yuri
  * @since 2021/12/15 11:06
  */
 public class Main {
     public static void main(String[] args) {
-        StringBuilder stringBuilder = new StringBuilder("adf");
-        StringBuilder reverse = stringBuilder.reverse();
+
+        String s = "a good   example";
+        String[] s1 = s.split(" ");
+        String ss = Arrays.stream(s1).filter(str -> !Objects.equals(str, "") && Objects.nonNull(str))
+                .collect(Collectors.joining(" "));
+        String sss = new StringBuffer(ss).reverse().toString();
+
+        System.out.println(sss);
     }
 }
