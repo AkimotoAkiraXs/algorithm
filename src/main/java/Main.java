b8d5aaa0b60918149ca121f3c26a682d1e11559e
Yuri <akimotoakiraxs@gmail.com>
2022-09-26 11:07:42 +0800

    535 TinyURL 的加密与解密_随机数

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+
 
 /**
  * @author Yuri
@@ -8,30 +6,8 @@ import java.util.Map;
  */
 public class Main {
     public static void main(String[] args) {
-        String[] strs = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        Map<Integer, String> map = new HashMap<>();
-        int N = 26;
 
-        int[][] cnts = new int[N][2];
-
-        for (int i = 0; i < N; i++) cnts[i][0] = i + 'a';
-        for (String str : strs) {
-            boolean[] b = new boolean[N];
-            for (char c : str.toCharArray()) {
-                int k = c - 'a';
-                if (!b[k]) {
-                    b[k] = true;
-                    cnts[k][1]++;
-                }
-            }
-        }
-        Arrays.sort(cnts, (a, b) -> {
-            if (a[1] == 0 && b[1] != 0) return 1;
-            if (a[1] != 0 && b[1] == 0) return -1;
-            if (a[1] == b[1]) return a[0] - b[0];
-            return a[1] - b[1];
-        });
-
-        System.out.println(Arrays.deepToString(cnts));
+        double random = Math.random();
+        System.out.println(random);
     }
 }
