b273830e3279c9563c155c149f806253484c44a3
Yuri <akimotoakiraxs@gmail.com>
2022-09-28 14:06:21 +0800

    640 求解方程_模拟 正则方式探索 耗时记录

--- src/main/java/leetcode/editor/cn/SolveTheEquation.java
+++ src/main/java/leetcode/editor/cn/SolveTheEquation.java
@@ -65,7 +65,7 @@ public class SolveTheEquation {
 
         public String solveEquation(String equation) {
             equation = equation.replace("-", "+-");
-//            equation = equation.replaceAll("(\\D)x|^x", "$11x"); // 可以利用正则替换x,-x为1x,-1x，但是会慢很多
+            equation = equation.replaceAll("(\\D)x|^x", "$11x"); // 可以利用正则替换x,-x为1x,-1x，但是会慢很多。 7ms 7.07%
             int e = equation.indexOf("=");
             String[] left = equation.substring(0, e).split("\\+");
             String[] right = equation.substring(e + 1).split("\\+");
@@ -77,7 +77,7 @@ public class SolveTheEquation {
         void calculate(String[] strings, int operative) {
             for (String str : strings) {
                 if (Objects.equals(str, "")) continue;
-                // 做if判断会比正则快很多
+/*                // 做if判断会比正则快很多 3ms 91.2%
                 if (str.equals("x")) {
                     x += operative;
                     continue;
@@ -85,7 +85,7 @@ public class SolveTheEquation {
                 if (str.equals("-x")) {
                     x -= operative;
                     continue;
-                }
+                }*/
                 boolean xFlag = false;
                 if (str.charAt(str.length() - 1) == 'x') {
                     xFlag = true;
