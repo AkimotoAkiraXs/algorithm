e545c7a1147acf01b03e550ccf941abd104915c0
Yuri <akimotoakiraxs@gmail.com>
2023-02-13 10:56:22 +0800

    tips 判断整数是否为2的幂

---
+++ src/main/java/tips/JudgePowerOf2.java
@@ -0,0 +1,22 @@
+package tips;
+
+import lombok.Data;
+
+/**
+ * 判断整数是否为2的幂
+ *
+ * @author Yuri
+ * @since 2023/2/13 10:52
+ */
+@Data
+public class JudgePowerOf2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean judge(int num) {
+        // 利用计算机二进制
+        return (num & (num - 1)) == 0;
+    }
+}
