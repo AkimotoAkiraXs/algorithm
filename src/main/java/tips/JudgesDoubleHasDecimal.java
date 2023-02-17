f373721e2a6f0b9abb97cd9f476eb1eca64e512b
Yuri <akimotoakiraxs@gmail.com>
2023-02-17 21:33:32 +0800

    tips JudgesDoubleHasDecimal代码优化

--- src/main/java/tips/JudgesDoubleHasDecimal.java
+++ src/main/java/tips/JudgesDoubleHasDecimal.java
@@ -9,7 +9,7 @@ import lombok.Data;
  * @since 2023/2/12 5:35
  */
 @Data
-public class JudgeFloatHasDecimal {
+public class JudgesDoubleHasDecimal {
 
     public static void main(String[] args) {
         System.out.println(judge(1.1));
@@ -25,5 +25,7 @@ public class JudgeFloatHasDecimal {
         // 精度保证
         double eps = 1e-10; //设置精度，因为计算机中double存在精度丢失，比如：1.000000000000000012
         return num - Math.floor(num) < eps; //存在精度丢失所以不直接判断num-Math.floor(num) > 0
+
+        // 不考虑精度可以直接 return num - (int)num
     }
 }
