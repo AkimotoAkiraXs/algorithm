702220926888940aba4ba5bc21316226943f0956
Yuri <akimotoakiraxs@gmail.com>
2023-02-17 19:45:29 +0800

    华为od机试 1~5

---
+++ src/main/java/nowcoder/hj/Hj1.java
@@ -0,0 +1,18 @@
+package nowcoder.hj;
+
+import java.util.Scanner;
+
+/**
+ * @author Yuri
+ * @since 2023/2/17 18:43
+ */
+
+public class Hj1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        String s = in.nextLine();
+        String[] ary = s.split(" ");
+        System.out.println(ary[ary.length - 1].length());
+    }
+}
