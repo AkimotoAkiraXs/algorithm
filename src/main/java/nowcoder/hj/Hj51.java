6cc3a926adec5b9213d518d365f6a92045ad488b
Yuri <akimotoakiraxs@gmail.com>
2023-03-13 11:13:21 +0800

    hj 51~57

---
+++ src/main/java/nowcoder/hj/Hj51.java
@@ -0,0 +1,21 @@
+package nowcoder.hj;
+
+import java.util.Scanner;
+
+/**
+ * @author Yuri
+ * @since 2023/3/12 11:05
+ */
+
+public class Hj51 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            int n = in.nextInt();
+            in.nextLine(); // 吃掉换行符
+            String s = in.nextLine();
+            String[] nodes = s.split(" ");
+            System.out.println(nodes[n - in.nextInt()]);
+        }
+    }
+}
---
+++ src/main/java/nowcoder/hj/Hj53.java
@@ -0,0 +1,23 @@
+package nowcoder.hj;
+
+import java.util.Scanner;
+
+/**
+ * @author Yuri
+ * @since 2023/3/13 10:04
+ */
+
+public class Hj53 {
+    // 数据范围1≤n≤10^9，所以暴力以模拟时不合理的，此题肯定是找规律题，通过多画几行能发现规律是2 3 2 4，第一行和第二行没有偶数
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] law = {2, 3, 2, 4}; // 规律
+        while (in.hasNextInt()) {
+            int n = in.nextInt() - 1;
+            int res;
+            if (n == 0 || n == 1) res = -1;
+            else res = law[(n - 2) % 4];
+            System.out.println(res);
+        }
+    }
+}
