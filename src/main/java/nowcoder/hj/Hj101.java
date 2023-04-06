d01dc785757c001c7d593abcb80c8d3a22e1c466
Yuri <akimotoakiraxs@gmail.com>
2023-04-06 23:54:59 +0800

    Hj99~101

---
+++ src/main/java/nowcoder/hj/Hj101.java
@@ -0,0 +1,20 @@
+package nowcoder.hj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Yuri
+ * @since 2023/4/6 23:27
+ */
+
+public class Hj101 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.nextLine();
+        String[] s = in.nextLine().split(" ");
+        int k = in.nextInt();
+        Arrays.stream(s).map(Integer::parseInt).sorted((o1, o2) -> k == 0 ? o1 - o2 : o2 - o1)
+                .forEach(o -> System.out.printf("%d ", o));
+    }
+}
---
+++ src/main/java/nowcoder/hj/Hj102.java
@@ -0,0 +1,27 @@
+package nowcoder.hj;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * @author Yuri
+ * @since 2023/4/6 23:32
+ */
+
+public class Hj102 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Map<String, Long> map = Arrays.stream(in.nextLine().split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        map.entrySet().stream().sorted((o1, o2) -> {
+            if (Objects.equals(o1.getValue(), o2.getValue())) {
+                return o1.getKey().compareTo(o2.getKey());
+            }
+            return (int) (o2.getValue() - o1.getValue());
+        }).forEach(o -> System.out.printf("%s", o.getKey()));
+
+    }
+}
