19438495962efe065c423be9fa1a2dbfca924dd3
Yuri <akimotoakiraxs@gmail.com>
2023-04-07 17:20:39 +0800

    牛顿迭代公式

---
+++ src/main/java/algorithm/NewtonMethod.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+/**
+ * <p>牛顿迭代公式：以一种无限逼近方程式结果的迭代公式来求得近似值
+ *
+ * <p>求解三次方方程 {@link nowcoder.hj.Hj107 求解立方根}
+ *
+ * @author Yuri
+ * @see <a href="https://zh.wikipedia.org/wiki/%E7%89%9B%E9%A1%BF%E6%B3%95">wiki 牛顿迭代法</a>
+ * @since 2023/4/7 17:11
+ */
+public class NewtonMethod {
+
+}
