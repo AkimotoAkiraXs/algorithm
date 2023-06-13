af2745406b4ddf068e38b6252ce26fcad3174b86
Yuri <akimotoakiraxs@gmail.com>
2023-06-13 23:32:56 +0800

    [Tep] 新增Lc竞赛模板

---
+++ src/main/java/leetcode/template/Contest3.java
@@ -0,0 +1,22 @@
+package leetcode.template;
+
+/**
+ * @author Yuri
+ * @since 2023/6/11 0:02
+ */
+
+public class Contest3 {
+
+    public static void main(String[] args) {
+        Solution solution = new Contest3().new Solution();
+
+
+
+        System.out.println();
+    }
+
+    class Solution {
+
+    }
+
+}
