603c48a43e38d9f162a0c22f4a7d3a0aa0d443a7
Yuri <akimotoakiraxs@gmail.com>
2023-06-26 22:05:30 +0800

    [CT] Lc单周赛350（1356 / 3580） Q1 2739.总行驶距离_模拟、数学|AC Q2 2740.找出分区值_模拟|AC Q3 2741.特别的排列_状压Dp Q4 2742.给墙壁刷油漆_01背包

---
+++ src/main/java/leetcode/editor/cn/FindTheValueOfThePartition.java
@@ -0,0 +1,34 @@
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+
+/**
+ * Id：&emsp;&emsp;2740 <br/>
+ * Name：Find the Value of the Partition <br/>
+ * <p>
+ * 单周赛350 Q2
+ *
+ * @author Yuri
+ * @since 2023-06-20 21:51:02
+ */
+
+public class FindTheValueOfThePartition {
+    public static void main(String[] args) {
+        Solution solution = new FindTheValueOfThePartition().new Solution();
+        System.out.println();
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        // 题意理解题
+        public int findValueOfPartition(int[] nums) {
+            Arrays.sort(nums);
+            int min = Integer.MAX_VALUE;
+            for (int i = 1; i < nums.length; i++)
+                min = Math.min(nums[i] - nums[i - 1], min);
+            return min;
+        }
+    }
+// leetcode submit region end(Prohibit modification and deletion)
+
+}
