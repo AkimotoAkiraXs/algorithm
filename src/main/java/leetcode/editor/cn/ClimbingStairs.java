c8e323c6d5dff5314e26a663b3b9ea5dd988919a
Yuri <akimotoakiraxs@gmail.com>
2023-06-27 15:48:00 +0800

    [AC] Lc70 爬楼梯_dp、数学（斐波那契数列通项公式）

--- src/main/java/leetcode/editor/cn/ClimbingStairs.java
+++ src/main/java/leetcode/editor/cn/ClimbingStairs.java
@@ -1,4 +1,4 @@
-//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。 
+// 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
 //
 // 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？ 
 //
@@ -7,19 +7,19 @@
 // 示例 1： 
 //
 // 输入： 2
-//输出： 2
-//解释： 有两种方法可以爬到楼顶。
-//1.  1 阶 + 1 阶
-//2.  2 阶 
+// 输出： 2
+// 解释： 有两种方法可以爬到楼顶。
+// 1.  1 阶 + 1 阶
+// 2.  2 阶
 //
 // 示例 2： 
 //
 // 输入： 3
-//输出： 3
-//解释： 有三种方法可以爬到楼顶。
-//1.  1 阶 + 1 阶 + 1 阶
-//2.  1 阶 + 2 阶
-//3.  2 阶 + 1 阶
+// 输出： 3
+// 解释： 有三种方法可以爬到楼顶。
+// 1.  1 阶 + 1 阶 + 1 阶
+// 2.  1 阶 + 2 阶
+// 3.  2 阶 + 1 阶
 // 
 // Related Topics 记忆化搜索 数学 动态规划 
 // 👍 1918 👎 0
@@ -38,9 +38,9 @@ public class ClimbingStairs {
         System.out.println("Hello world");
     }
 
-    //leetcode submit region begin(Prohibit modification and deletion)
+    // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        //时间复杂度O(n)
+        // 时间复杂度O(n)
         public int climbStairs(int n) {
             int p, q = 0, r = 1;
             for (int i = 1; i <= n; i++) {
@@ -50,19 +50,18 @@ public class ClimbingStairs {
             }
             return r;
         }
-/*
-        //斐波那契数列的通项公式
-        public int climbStairs(int n) {
+
+        public int climbStairs_Dfs(int n) {
+            if (n <= 2) return n;
+            return climbStairs_Dfs(n - 1) + climbStairs_Dfs(n - 2);
+        }
+
+        // 斐波那契数列的通项公式
+        public int climbStairs_Math(int n) {
             double sqrt5 = Math.sqrt(5);
             double fn = Math.pow((1 + sqrt5) / 2, n + 1) - Math.pow((1 - sqrt5) / 2, n + 1);
             return (int) Math.round(fn / sqrt5);
-        }*/
-
-        public int dfs(int n) {
-            if (n == 1) return 1;
-            return 1 + dfs(n - 1);
         }
-
     }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
 } 
