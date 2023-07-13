758e9804990d366a5b81f494514e505e2ae1e7a2
Yuri <akimotoakiraxs@gmail.com>
2023-07-13 16:21:23 +0800

    [tip] 位运算：获取一个数转二进制后的长度

--- src/main/java/tips/BitAlgorithm.java
+++ src/main/java/tips/BitAlgorithm.java
@@ -22,7 +22,7 @@ public class BitAlgorithm {
      * @param num 需要判断的整数
      * @return 整数是否为2的幂
      */
-    public static boolean isPowerOfTwo(int num) {
+    private static boolean isPowerOfTwo(int num) {
         // 利用计算机二进制
         return (num & (num - 1)) == 0;
     }
@@ -33,7 +33,7 @@ public class BitAlgorithm {
      * @param num 需要处理的整数
      * @return 整数2进制下的最低位
      */
-    public static int getLowerBitOfTwo(int num) {
+    private static int getLowerBitOfTwo(int num) {
         return num - (num & (num - 1));
     }
 
@@ -43,8 +43,26 @@ public class BitAlgorithm {
      * @param num 需要处理的整数
      * @return 整数2进制下的最高位
      */
-    public static int getHigherBitOfTwo(int num) {
+    private static int getHigherBitOfTwo(int num) {
         while ((num & (num - 1)) != 0) num &= num - 1;
         return num;
+    }
+
+    /**
+     * 获取一个数的二进制长度
+     *
+     * @param num 需要处理的整数
+     * @return num转2进制后的长度
+     */
+    private static int getLenOfBit(int num) {
+        int len = 0;
+        for (int i = 31; i > 0; i--) {
+            if (num >> i == 1) {
+                len = i;
+                break;
+            }
+        }
+        return len;
+        // return Integer.toBinaryString(num).length();
     }
 }
