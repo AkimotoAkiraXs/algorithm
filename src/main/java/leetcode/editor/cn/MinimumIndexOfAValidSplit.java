974429680cdfe0a7a2a19742f708f7a9cb53afe9
Yuri <akimotoakiraxs@gmail.com>
2023-07-18 23:23:31 +0800

    [CT] Lc单周赛354（1736/3957） Q1 Lc2778.特殊元素平方和_模拟|AC Q2 Lc2779.数组的最大美丽值_滑动窗口+双指针 Q3 Lc2790.合法分割的最小下标_模拟|AC Q4 Lc2781.最长合法子字符串的长度_双指针

---
+++ src/main/java/leetcode/editor/cn/MinimumIndexOfAValidSplit.java
@@ -0,0 +1,43 @@
+package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Id：&emsp;&emsp;2780 <br/>
+ * Name：Minimum Index of a Valid Split <br/>
+ *
+ * @author Yuri
+ * @since 2023-07-18 21:56:03
+ */
+
+public class MinimumIndexOfAValidSplit {
+    public static void main(String[] args) {
+        Solution solution = new MinimumIndexOfAValidSplit().new Solution();
+        System.out.println();
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        // 可以推出个结论 原数组支配元素和拆后支配元素是同一个
+        public int minimumIndex(List<Integer> nums) {
+            Map<Integer, Long> map = nums.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+            Map<Integer, Long> fMap = new HashMap<>();
+            int m = nums.size();
+            for (int i = 0; i < m - 1; i++) {
+                Integer integer = nums.get(i);
+                fMap.merge(integer, 1L, Long::sum);
+                Long t = fMap.get(integer);
+                Long all = map.get(integer);
+                if (t * 2 > i + 1 && (all - t) * 2 > m - i - 1) return i;
+            }
+            return -1;
+        }
+    }
+// leetcode submit region end(Prohibit modification and deletion)
+
+}
