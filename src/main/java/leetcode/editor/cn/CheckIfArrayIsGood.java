311497e44c3ba8089a7577b8eee8511724094b4d
Yuri <akimotoakiraxs@gmail.com>
2023-07-26 00:36:21 +0800

    [CT] Lc双周赛109（3981/24639） Q1 Lc2784.检查数组是否是好的_模拟|AC Q2 Lc2785.将字符串中的元音字母排序_模拟+hash、位运算|AC Q3 Lc2786.访问数组中的位置使分数最大_Dp|AC Q4 Lc2787.将一个数字表示成幂的和的方案数_Dp（标准01背包）|LOSE

---
+++ src/main/java/leetcode/editor/cn/CheckIfArrayIsGood.java
@@ -0,0 +1,30 @@
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+
+/**
+ * Id：&emsp;&emsp;2784 <br/>
+ * Name：Check if Array is Good <br/>
+ *
+ * @author Yuri
+ * @since 2023-07-25 23:47:22
+ */
+
+public class CheckIfArrayIsGood {
+    public static void main(String[] args) {
+        Solution solution = new CheckIfArrayIsGood().new Solution();
+        System.out.println();
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isGood(int[] nums) {
+            Arrays.sort(nums);
+            int n = nums.length;
+            for (int i = 1; i < nums.length; i++) if (nums[i - 1] != i) return false;
+            return nums[n - 1] == n - 1;
+        }
+    }
+// leetcode submit region end(Prohibit modification and deletion)
+
+}
