1f9f4682af5382b70ec98b1ca438e4755b672d83
Yuri <akimotoakiraxs@gmail.com>
2023-08-14 19:26:00 +0800

    [AC] Lc617 合并二叉树_dfs

--- src/main/code/leetcode/editor/cn/MergeTwoBinaryTrees.java
+++ src/main/code/leetcode/editor/cn/MergeTwoBinaryTrees.java
@@ -1,4 +1,4 @@
-//给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。 
+// 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
 //
 // 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点
 //。 
@@ -6,15 +6,15 @@
 // 示例 1: 
 //
 // 
-//输入: 
+// 输入:
 //	Tree 1                     Tree 2                  
 //          1                         2                             
 //         / \                       / \                            
 //        3   2                     1   3                        
 //       /                           \   \                      
 //      5                             4   7                  
-//输出: 
-//合并后的树:
+// 输出:
+// 合并后的树:
 //	     3
 //	    / \
 //	   4   5
@@ -46,7 +46,7 @@ public class MergeTwoBinaryTrees {
         solution.mergeTrees(node1, node2);
 
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
+    // leetcode submit region begin(Prohibit modification and deletion)
 
     /**
      * Definition for a binary tree node.
@@ -66,17 +66,14 @@ public class MergeTwoBinaryTrees {
     class Solution {
 
         public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-            if (root1 == null && root2 == null) return null;
-            else if (root1 == null) return root2;
-            else if (root2 == null) return root1;
-            else {
-                TreeNode node = new TreeNode();
-                node.val = root1.val + root2.val;
-                node.left = mergeTrees(root1.left, root2.left);
-                node.right = mergeTrees(root1.right, root2.right);
-                return node;
-            }
+            if (root1 == null) return root2;
+            if (root2 == null) return root1;
+            TreeNode node = new TreeNode();
+            node.val = root1.val + root2.val;
+            node.left = mergeTrees(root1.left, root2.left);
+            node.right = mergeTrees(root1.right, root2.right);
+            return node;
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
 }
