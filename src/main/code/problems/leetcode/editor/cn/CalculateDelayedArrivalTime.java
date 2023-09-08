5d31b2eea37c71510ec90bdbbd904fe7ec59bcdc
Yuri <akimotoakiraxs@gmail.com>
2023-09-08 18:29:08 +0800

    [AC] Lc2651 计算列车到站时间_数学

---
+++ src/main/code/problems/leetcode/editor/cn/CalculateDelayedArrivalTime.java
@@ -0,0 +1,65 @@
+// You are given a positive integer arrivalTime denoting the arrival time of a
+// train in hours, and another positive integer delayedTime denoting the amount of
+// delay in hours.
+//
+// Return the time when the train will arrive at the station. 
+//
+// Note that the time in this problem is in 24-hours format. 
+//
+// 
+// Example 1: 
+//
+// 
+// Input: arrivalTime = 15, delayedTime = 5
+// Output: 20
+// Explanation: Arrival time of the train was 15:00 hours. It is delayed by 5
+// hours. Now it will reach at 15+5 = 20 (20:00 hours).
+// 
+//
+// Example 2: 
+//
+// 
+// Input: arrivalTime = 13, delayedTime = 11
+// Output: 0
+// Explanation: Arrival time of the train was 13:00 hours. It is delayed by 11
+// hours. Now it will reach at 13+11=24 (Which is denoted by 00:00 in 24 hours
+// format so return 0).
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= arrivaltime < 24 
+// 1 <= delayedTime <= 24 
+// 
+//
+// 👍 63 👎 0
+
+package problems.leetcode.editor.cn;
+
+/**
+ * Id：&emsp;&emsp;2651
+ * <p>
+ * Name：Calculate Delayed Arrival Time
+ *
+ * @author Yuri
+ * @since 2023-09-08 18:25:55
+ */
+
+
+public class CalculateDelayedArrivalTime {
+    public static void main(String[] args) {
+        Solution solution = new CalculateDelayedArrivalTime().new Solution();
+
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+            return (arrivalTime + delayedTime) % 24;
+        }
+    }
+// leetcode submit region end(Prohibit modification and deletion)
+
+} 
