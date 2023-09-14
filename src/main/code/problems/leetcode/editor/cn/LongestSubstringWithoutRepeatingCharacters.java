454fd146d1b2bb99163f0f19ecd796f960668c61
Yuri <akimotoakiraxs@gmail.com>
2023-09-14 21:26:56 +0800

    [AC] Lc3 无重复字符的最长子串_滑动窗口（不定长）

--- src/main/code/problems/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/code/problems/leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,37 +1,37 @@
-//给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。 
+// 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
 //
 // 
 //
 // 示例 1: 
 //
 // 
-//输入: s = "abcabcbb"
-//输出: 3 
-//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 输入: s = "abcabcbb"
+// 输出: 3
+// 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
 // 
 //
 // 示例 2: 
 //
 // 
-//输入: s = "bbbbb"
-//输出: 1
-//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 输入: s = "bbbbb"
+// 输出: 1
+// 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
 // 
 //
 // 示例 3: 
 //
 // 
-//输入: s = "pwwkew"
-//输出: 3
-//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+// 输入: s = "pwwkew"
+// 输出: 3
+// 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
 //     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
 // 
 //
 // 示例 4: 
 //
 // 
-//输入: s = ""
-//输出: 0
+// 输入: s = ""
+// 输出: 0
 // 
 //
 // 
@@ -56,26 +56,24 @@ package problems.leetcode.editor.cn;
 public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
         Solution solution = new LongestSubstringWithoutRepeatingCharacters().new Solution();
-        solution.lengthOfLongestSubstring("aabaab!bb");
+        solution.lengthOfLongestSubstring("dvdf");
     }
 
-    //leetcode submit region begin(Prohibit modification and deletion)
+    // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
         public int lengthOfLongestSubstring(String s) {
-            StringBuilder stringBuilder = new StringBuilder(s);
-            StringBuilder str = new StringBuilder();
-            int max = 0;
-            for (int i = 0; i < s.length(); i++) {
-                char c = stringBuilder.charAt(i);
-                int index = str.indexOf(String.valueOf(c));
-                if (index != -1) {
-                    str.delete(0, index+1);
-                }
-                str.append(c);
-                max = Math.max(max, str.length());
+            char[] cs = s.toCharArray();
+            int[] cnt = new int[128];
+            int n = cs.length;
+            int l = 0, max = 0;
+            for (int r = 0; r < n; r++) {
+                cnt[cs[r]]++;
+                while (cnt[cs[r]] > 1) cnt[cs[l++]]--;
+                max = Math.max(max, r - l + 1);
             }
             return max;
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
+    // leetcode submit region end(Prohibit modification and deletion)
 } 
