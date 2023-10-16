f112f7d7ac9318885c8bf6b6f83d17f08c3e2347
Yuri <akimotoakiraxs@gmail.com>
2023-10-16 18:47:26 +0800

    [Alg] 位运算-获取最低位1

--- src/main/code/algorithm/BitAlgorithm.java
+++ src/main/code/algorithm/BitAlgorithm.java
@@ -1,4 +1,4 @@
-package tips;
+package algorithm;
 
 import lombok.Data;
 
@@ -19,50 +19,52 @@ public class BitAlgorithm {
     /**
      * 判断整数是否为2的幂
      *
-     * @param num 需要判断的整数
+     * @param k 需要判断的整数
      * @return 整数是否为2的幂
      */
-    private static boolean isPowerOfTwo(int num) {
+    private static boolean isPowerOfTwo(int k) {
         // 利用计算机二进制
-        return (num & (num - 1)) == 0;
+        return (k & (k - 1)) == 0;
     }
 
     /**
      * 获取整数2进制最低位（右位）
      *
-     * @param num 需要处理的整数
+     * @param k 需要处理的整数
      * @return 整数2进制下的最低位
      */
-    private static int getLowerBitOfTwo(int num) {
-        return num - (num & (num - 1));
+    private static int getLowerBitOfTwo(int k) {
+        return k - (k & (k - 1));
+        // return k & -k;
+        // return k & (k-1) ^ k
     }
 
     /**
      * 获取整数2进制最高位（左位）
      *
-     * @param num 需要处理的整数
+     * @param k 需要处理的整数
      * @return 整数2进制下的最高位
      */
-    private static int getHigherBitOfTwo(int num) {
-        while ((num & (num - 1)) != 0) num &= num - 1;
-        return num;
+    private static int getHigherBitOfTwo(int k) {
+        while ((k & (k - 1)) != 0) k &= k - 1;
+        return k;
     }
 
     /**
      * 获取一个数的二进制长度
      *
-     * @param num 需要处理的整数
+     * @param k 需要处理的整数
      * @return num转2进制后的长度
      */
-    private static int getLenOfBit(int num) {
+    private static int getLenOfBit(int k) {
         int len = 0;
         for (int i = 31; i > 0; i--) {
-            if (num >> i == 1) {
+            if (k >> i == 1) {
                 len = i;
                 break;
             }
         }
         return len;
-        // return Integer.toBinaryString(num).length();
+        // return Integer.toBinaryString(k).length();
     }
 }
