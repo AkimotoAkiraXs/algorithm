23541a7767e515afb9180266d403b407e74f277d
Yuri <akimotoakiraxs@gmail.com>
2023-10-19 16:44:37 +0800

    [Tep] 二分查找模板更新

--- src/main/code/template/BinarySearchTemplate.java
+++ src/main/code/template/BinarySearchTemplate.java
@@ -8,23 +8,29 @@ package template;
  */
 
 public class BinarySearchTemplate {
+
     public static void main(String[] args) {
+        BinarySearchTemplate bst = new BinarySearchTemplate();
+        System.out.println(bst.binarySearch(new int[]{1, 2, 3, 4, 4, 4, 5, 5, 5, 6, 8, 9, 9, 9, 9}, 7));
 
     }
 
     /**
-     * 二分查找：在nums中搜索大于key的第一个位置，当其为最小时返回0，大于等于最大值时返回数组长度
+     * 二分查找：只需要记一个模板就可以了，该模板为左闭右开
      * <p>
-     * 返回值-1就表示<=key的第一个位置
+     * 对于求小于key的位置，大于key的最后一个位置等问题都可以通过等价转换得到。
+     *
+     * @return 大于/大于等于key的第一个位置，当key小于最小值时返回0，大于/大于等于最大值时返回数组长度n
      */
-    int biSearchLeft(int[] nums, int key) {
+    private int binarySearch(int[] nums, int key) {
         int l = 0, r = nums.length;
         while (l < r) {
             int mid = (l + r) / 2;
-            if (nums[mid] <= key) l = mid + 1;
+            if (nums[mid] <= key) l = mid + 1; // 去掉等号则会找大于等于，加上等号则会只找大于
             else r = mid;
         }
         return l;
     }
+
 
 }
