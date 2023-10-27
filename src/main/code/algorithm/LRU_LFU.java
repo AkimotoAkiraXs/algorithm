87f5373d2c8c2ef36316b205367b568c8f49d90b
Yuri <akimotoakiraxs@gmail.com>
2023-10-27 18:34:23 +0800

    [Alg] LRU、LFU

---
+++ src/main/code/algorithm/LRU_LFU.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import problems.leetcode.editor.cn.LfuCache;
+import problems.leetcode.editor.cn.LruCache;
+
+/**
+ * LRU(Least Recently Used)-最近最少使用算法：当到达缓存上限时，删除最久位使用的节点。
+ * <p>
+ * LFU(least frequently used)-最不经常使用算法：当到达缓存上限时，删除最低频率被访问的节点，频率相同时删除最久问使用的节点。
+ *
+ * @author Yuri
+ * @see LruCache Lc146.LRU Cache
+ * @see LfuCache Lc460.LFU Cache
+ * @since 2023/10/27 18:28
+ */
+
+
+public class LRU_LFU {
+
+}
