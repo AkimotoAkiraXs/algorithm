9472a77201f042964eb913c5ffbc102cf9736f20
Yuri <akimotoakiraxs@gmail.com>
2024-01-25 15:18:00 +0800

    [AC] Lc2859 计算K置位下标对应元素的和_模拟

---
+++ src/main/code/problems/leetcode/editor/cn/SumOfValuesAtIndicesWithKSetBits.java
@@ -0,0 +1,86 @@
+// You are given a 0-indexed integer array nums and an integer k.
+//
+// Return an integer that denotes the sum of elements in nums whose 
+// corresponding indices have exactly k set bits in their binary representation.
+//
+// The set bits in an integer are the 1's present when it is written in binary. 
+//
+//
+// 
+// For example, the binary representation of 21 is 10101, which has 3 set bits. 
+//
+// 
+//
+// 
+// Example 1: 
+//
+// 
+// Input: nums = [5,10,1,5,2], k = 1
+// Output: 13
+// Explanation: The binary representation of the indices are:
+// 0 = 0002
+// 1 = 0012
+// 2 = 0102
+// 3 = 0112
+// 4 = 1002
+// Indices 1, 2, and 4 have k = 1 set bits in their binary representation.
+// Hence, the answer is nums[1] + nums[2] + nums[4] = 13.
+//
+// Example 2: 
+//
+// 
+// Input: nums = [4,3,2,1], k = 2
+// Output: 1
+// Explanation: The binary representation of the indices are:
+// 0 = 002
+// 1 = 012
+// 2 = 102
+// 3 = 112
+// Only index 3 has k = 2 set bits in its binary representation.
+// Hence, the answer is nums[3] = 1.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums.length <= 1000 
+// 1 <= nums[i] <= 10⁵ 
+// 0 <= k <= 10 
+// 
+//
+// 👍 25 👎 0
+
+package problems.leetcode.editor.cn;
+
+import java.util.List;
+
+/**
+ * Id：&emsp;&emsp;2859
+ * <p>
+ * Name：Sum of Values at Indices With K Set Bits
+ *
+ * @author Yuri
+ * @since 2024-01-25 15:06:57
+ */
+
+public class SumOfValuesAtIndicesWithKSetBits {
+
+    public static void main(String[] args) {
+        Solution solution = new SumOfValuesAtIndicesWithKSetBits().new Solution();
+
+    }
+
+    // leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+            int ans = 0;
+            for (int i = 0; i < nums.size(); i++)
+                if (Integer.bitCount(i) == k) ans += nums.get(i);
+            return ans;
+        }
+    }
+// leetcode submit region end(Prohibit modification and deletion)
+
+}
