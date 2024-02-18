ae64dc3961f786fbbe5826c2ba694436815679b2
Yuri <akimotoakiraxs@gmail.com>
2024-02-18 11:28:30 +0800

    [AC] Lc162 寻找峰值_二分法

--- src/main/code/problems/leetcode/editor/cn/FindPeakElement.java
+++ src/main/code/problems/leetcode/editor/cn/FindPeakElement.java
@@ -1,4 +1,4 @@
-//峰值元素是指其值严格大于左右相邻值的元素。 
+// 峰值元素是指其值严格大于左右相邻值的元素。 
 //
 // 给你一个整数数组 nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。 
 //
@@ -11,16 +11,16 @@
 // 示例 1： 
 //
 // 
-//输入：nums = [1,2,3,1]
-//输出：2
-//解释：3 是峰值元素，你的函数应该返回其索引 2。 
+// 输入：nums = [1,2,3,1]
+// 输出：2
+// 解释：3 是峰值元素，你的函数应该返回其索引 2。 
 //
 // 示例 2： 
 //
 // 
-//输入：nums = [1,2,1,3,5,6,4]
-//输出：1 或 5 
-//解释：你的函数可以返回索引 1，其峰值元素为 2；
+// 输入：nums = [1,2,1,3,5,6,4]
+// 输出：1 或 5 
+// 解释：你的函数可以返回索引 1，其峰值元素为 2；
 //     或者返回索引 5， 其峰值元素为 6。
 // 
 //
@@ -44,43 +44,30 @@ package problems.leetcode.editor.cn;
  * Name：寻找峰值
  *
  * @author Yuri
+ * @see FindAPeakElementIi 寻找峰值II
  * @since 2021-12-24 14:23:56
  */
 public class FindPeakElement {
+
     public static void main(String[] args) {
         Solution solution = new FindPeakElement().new Solution();
         int[] nums = new int[]{6, 5, 4, 3, 2, 3, 2};
-        int peakElement = solution.findPeakElement(nums);
-        System.out.println(peakElement);
-        System.out.println();
+        System.out.println(solution.findPeakElement(nums));
     }
 
-    //leetcode submit region begin(Prohibit modification and deletion)
+    // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        // 题目要求随意给出一个峰值即可，且左右两边都为负无穷，所以用二分不断缩小范围，每次方向选更高的一方
         public int findPeakElement(int[] nums) {
-            int n = nums.length;
-            if (n == 1) {
-                return 0;
+            int left = 0, right = nums.length;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (nums[mid] < nums[mid + 1]) left = mid + 1;
+                else right = mid;
             }
-            int a = 0, b = n;
-            int mid = 0;
-            while (a <= b) {
-                mid = (a + b) >> 1;
-                if (mid == 0) {
-                    return nums[mid] > nums[mid + 1] ? mid : mid + 1;
-                } else if (mid == n - 1) {
-                    return nums[mid] > nums[mid - 1] ? mid : mid - 1;
-                } else if (nums[mid - 1] > nums[mid]) {
-                    b = mid - 1;
-                } else if (nums[mid + 1] > nums[mid]) {
-                    a = mid + 1;
-                } else {
-                    break;
-                }
-            }
-            return mid;
+            return left;
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
 
 }
