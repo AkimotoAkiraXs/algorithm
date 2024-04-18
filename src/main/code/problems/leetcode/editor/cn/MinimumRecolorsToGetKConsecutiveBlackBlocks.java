112803671e7c14c368327675af0bd73b9d7e63a6
Yuri <akimotoakiraxs@gmail.com>
2024-04-18 17:40:58 +0800

    [AC] Lc2379 得到K个黑块的最少涂色次数_滑动窗口

---
+++ src/main/code/problems/leetcode/editor/cn/MinimumRecolorsToGetKConsecutiveBlackBlocks.java
@@ -0,0 +1,85 @@
+//You are given a 0-indexed string blocks of length n, where blocks[i] is 
+//either 'W' or 'B', representing the color of the iᵗʰ block. The characters 'W' and 
+//'B' denote the colors white and black, respectively. 
+//
+// You are also given an integer k, which is the desired number of consecutive 
+//black blocks. 
+//
+// In one operation, you can recolor a white block such that it becomes a black 
+//block. 
+//
+// Return the minimum number of operations needed such that there is at least 
+//one occurrence of k consecutive black blocks. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: blocks = "WBBWWBBWBW", k = 7
+//Output: 3
+//Explanation:
+//One way to achieve 7 consecutive black blocks is to recolor the 0th, 3rd, and 
+//4th blocks
+//so that blocks = "BBBBBBBWBW". 
+//It can be shown that there is no way to achieve 7 consecutive black blocks in 
+//less than 3 operations.
+//Therefore, we return 3.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: blocks = "WBWBBBW", k = 2
+//Output: 0
+//Explanation:
+//No changes need to be made, since 2 consecutive black blocks already exist.
+//Therefore, we return 0.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// n == blocks.length 
+// 1 <= n <= 100 
+// blocks[i] is either 'W' or 'B'. 
+// 1 <= k <= n 
+// 
+//
+// 👍 125 👎 0
+
+package problems.leetcode.editor.cn;
+
+/**
+ * Id：&emsp;&emsp;2379
+ * <p>
+ * Name：Minimum Recolors to Get K Consecutive Black Blocks
+ *
+ * @author Yuri
+ * @since 2024-04-18 17:17:30
+ */
+
+public class MinimumRecolorsToGetKConsecutiveBlackBlocks {
+    public static void main(String[] args) {
+        Solution solution = new MinimumRecolorsToGetKConsecutiveBlackBlocks().new Solution();
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int minimumRecolors(String blocks, int k) {
+
+            int l = 0;
+            int min = Integer.MAX_VALUE;
+            int cnt = 0;
+            for (int r = 0; r < blocks.length(); r++) {
+                if (blocks.charAt(r) == 'W') cnt++;
+                if (r - l >= k && blocks.charAt(l++) == 'W') cnt--;
+                if (r - l == k - 1) min = Math.min(min, cnt);
+            }
+            return min;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
