baaaab862f3cf0d37f45e7e8a0602d269891dfbe
Yuri <akimotoakiraxs@gmail.com>
2024-05-31 18:18:05 +0800

    [Alg] 最小生成树：Prim（普里姆）

---
+++ src/main/code/algorithm/graph/Prim.java
@@ -0,0 +1,21 @@
+package algorithm.graph;
+
+import problems.leetcode.editor.cn.PathWithMinimumEffort;
+
+/**
+ * 普里姆
+ * <p>
+ * 基于Dijkstra生成最小生成树
+ * <p>
+ * 与Kruskal不同的是基于点进行排序，每次从堆中取出当前值最小的点，将其连接点加入堆中（类似于Dijkstra的思想）
+ *
+ * @author Yuri
+ * @see Kruskal 克鲁斯卡尔
+ * @see Dijkstra 迪杰斯特拉
+ * @see PathWithMinimumEffort Lc1631.最小体力消耗路径
+ * @since 2024/5/31 17:07
+ */
+
+public class Prim {
+
+}
