93b81a6616e7240835d07b428b4cb10264d5085c
Yuri <akimotoakiraxs@gmail.com>
2024-08-08 10:22:05 +0800

    [AC] Lc201 数字范围按位与_位运算

--- src/main/code/problems/leetcode/editor/cn/BitwiseAndOfNumbersRange.java
+++ src/main/code/problems/leetcode/editor/cn/BitwiseAndOfNumbersRange.java
@@ -1,4 +1,4 @@
-//给你两个整数 left 和 right ，表示区间 [left, right] ，返回此区间内所有数字 按位与 的结果（包含 left 、right 端点）
+// 给你两个整数 left 和 right ，表示区间 [left, right] ，返回此区间内所有数字 按位与 的结果（包含 left 、right 端点）
 //。 
 //
 // 
@@ -6,22 +6,22 @@
 // 示例 1： 
 //
 // 
-//输入：left = 5, right = 7
-//输出：4
+// 输入：left = 5, right = 7
+// 输出：4
 // 
 //
 // 示例 2： 
 //
 // 
-//输入：left = 0, right = 0
-//输出：0
+// 输入：left = 0, right = 0
+// 输出：0
 // 
 //
 // 示例 3： 
 //
 // 
-//输入：left = 1, right = 2147483647
-//输出：0
+// 输入：left = 1, right = 2147483647
+// 输出：0
 // 
 //
 // 
@@ -43,24 +43,37 @@
 package problems.leetcode.editor.cn;
 
 public class BitwiseAndOfNumbersRange {
+
     public static void main(String[] args) {
         Solution solution = new BitwiseAndOfNumbersRange().new Solution();
         System.out.println("Hello world");
     }
 
-    //leetcode submit region begin(Prohibit modification and deletion)
+    // leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public int rangeBitwiseAnd(int left, int right) {
-            if (left == right) return left;
-            String l = Integer.toBinaryString(left);
-            for (int i = l.length(); i > 0; i--) {
-                if (right >= Math.pow(2, i)) break;
 
+
+        /**
+         * 此题本质就是找公共前缀
+         */
+        public int rangeBitwiseAnd(int left, int right) {
+            while (left < right) right &= right - 1;
+            return right;
+        }
+
+        /**
+         * 以left高位到低位计算，如果right范围超过该位置，则该1约去
+         */
+        public int rangeBitwiseAnd_(int left, int right) {
+            int ans = 0;
+            while (left > 0) {
+                long high = Integer.highestOneBit(left);
+                if ((high << 1) + ans <= right) return ans;
+                ans ^= (int) high;
+                left ^= (int) high;
             }
-            int max = (int) Math.pow(2, l.length() + 1);
-            if (right >= max) return 0;
-            return 0;
+            return ans;
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
 } 
