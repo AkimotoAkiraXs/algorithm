15ffee944997bdd40233bafbaa61a04ed8ef96f9
Yuri <akimotoakiraxs@gmail.com>
2024-08-26 18:56:39 +0800

    [Tep] 二分搜索_左闭右闭

--- src/main/code/template/BinarySearchTemplate.java
+++ src/main/code/template/BinarySearchTemplate.java
@@ -11,25 +11,42 @@ public class BinarySearchTemplate {
 
     public static void main(String[] args) {
         BinarySearchTemplate bst = new BinarySearchTemplate();
-        System.out.println(bst.binarySearch(new int[]{1, 2, 3, 4, 4, 4, 5, 5, 5, 6, 8, 9, 9, 9, 9}, 7));
+        System.out.println(bst.binarySearch_left_close_right_close(new int[]{9, 9, 7, 7, 6, 5, 5, 2}, 7));
 
     }
 
     /**
-     * 二分查找：只需要记一个模板就可以了，该模板为左闭右开
+     * 左闭右开
      * <p>
-     * 对于求小于key的位置，大于key的最后一个位置等问题都可以通过等价转换得到。
+     * 适用于求最小值
      *
      * @return 大于/大于等于key的第一个位置，当key小于最小值时返回0，大于/大于等于最大值时返回数组长度n
      */
-    private int binarySearch(int[] nums, int key) {
+    private int binarySearch_left_open_right_close(int[] nums, int key) {
         int l = 0, r = nums.length;
         while (l < r) {
-            int mid = (l + r) / 2;
+            int mid = l + r >> 1;
             if (nums[mid] <= key) l = mid + 1; // 去掉等号则会找大于等于，加上等号则会只找大于
             else r = mid;
         }
         return l;
+    }
+
+    /**
+     * 左闭右闭
+     * <p>
+     * 适用于求最大值
+     *
+     * @return 返回大于key的最后一个数
+     */
+    private int binarySearch_left_close_right_close(int[] nums, int key) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + r >> 1;
+            if (nums[mid] > key) l = mid + 1; // 加上等于就会返回大于等于key的最后一个数
+            else r = mid - 1;
+        }
+        return r;
     }
 
 
